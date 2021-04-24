24dcb6e82e8f61902c686877570df5658ac7de52
hendrixt <hendrixt811@gmail.com>
2021-04-24 00:48:46 -0500

    assignment 7 commit, and fix for some files missing

---
+++ app/src/main/java/com/ualr/recyclerviewassignment/InboxListFragment.java
@@ -0,0 +1,143 @@
+package com.ualr.recyclerviewassignment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ualr.recyclerviewassignment.Utils.DataGenerator;
+import com.ualr.recyclerviewassignment.adapter.AdapterListBasic;
+
+import com.ualr.recyclerviewassignment.model.Inbox;
+import com.ualr.recyclerviewassignment.model.InboxViewModel;
+
+import java.util.List;
+
+public class InboxListFragment extends Fragment implements AdapterListBasic.OnItemClickListener {
+    private static final int DEFAULT_POS = 0;
+    private static final String TAG = InboxListFragment.class.getSimpleName();
+    private AdapterListBasic mAdapter;
+    private RecyclerView recyclerView;
+    private Context mContext;
+    private InboxViewModel viewModel;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+
+    @Override
+    public void onDetach() {
+
+        super.onDetach();
+
+        mContext = null;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+
+    @Override
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.inbox_list_fragment, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+
+        List<Inbox> items = DataGenerator.getInboxData(mContext);
+
+        viewModel = new ViewModelProvider(getActivity()).get(InboxViewModel.class);
+
+        viewModel.setInbox(items);
+
+        mAdapter = new AdapterListBasic(mContext, viewModel.getInboxList().getValue());
+        mAdapter.setOnItemClickListener(this);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(mContext);
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerView.setAdapter(mAdapter);
+
+        viewModel.getInboxList().observe(getViewLifecycleOwner(), new Observer<List<Inbox>>() {
+            @Override
+            public void onChanged(List<Inbox> inboxes) {
+
+                mAdapter.updateItems(inboxes);
+            }
+        });
+    }
+
+    @Override
+    public void onItemClick(View view, Inbox obj, int position) {
+
+        mAdapter.clearAllSelections();
+
+        obj.toggleSelection();
+
+        mAdapter.notifyItemChanged(position);
+
+        viewModel.setSelectedIndex(position);
+
+
+    }
+
+
+    public void addInboxItem(){
+
+        mAdapter.addItem(DEFAULT_POS,DataGenerator.getRandomInboxItem(mContext));
+        recyclerView.scrollToPosition(0);
+    }
+
+    public void removeInboxItem(){
+        int currentItem = viewModel.getSelectedIndex().getValue();
+
+        List<Inbox> current = viewModel.getInboxList().getValue();
+
+
+        if (currentItem != -1 && current != null){
+            mAdapter.removeItem(currentItem);
+        }
+
+    }
+
+    public void forwardEmail() {
+        ForwardDialogFragment forwardedFragment = ForwardDialogFragment.newInstance(viewModel.getSelectedIndex().getValue());
+
+        forwardedFragment.show(getFragmentManager(), TAG);
+
+        mAdapter.clearAllSelections();
+
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/ualr/recyclerviewassignment/adapter/AdapterListBasic.java
@@ -0,0 +1,156 @@
+package com.ualr.recyclerviewassignment.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ualr.recyclerviewassignment.R;
+import com.ualr.recyclerviewassignment.Utils.DataGenerator;
+import com.ualr.recyclerviewassignment.model.Inbox;
+
+import java.util.List;
+
+public class AdapterListBasic extends RecyclerView.Adapter {
+    private List<Inbox> mItems;
+    private Context mContext;
+    private OnItemClickListener mListener;
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, Inbox obj, int position);
+
+    }
+
+    public void setOnItemClickListener(final OnItemClickListener itemClickListener) {
+        this.mListener = itemClickListener;
+    }
+
+    public AdapterListBasic(Context context, List<Inbox> items) {
+        this.mItems = items;
+        this.mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder vh;
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.inbox_list, parent,false);
+        vh = new ItemViewHolder(itemView);
+
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
+        ItemViewHolder itemViewHolder = (ItemViewHolder) holder;
+        final Inbox inbox = mItems.get(position);
+
+        // Color resources
+        int selectedColor = mContext.getResources().getColor(R.color.grey_20);
+        int selectedIconColor = mContext.getResources().getColor(R.color.blue_300);
+        int defaultIconColor = mContext.getResources().getColor(R.color.blue_grey_600);
+
+        // Drawable resources and initializations
+        Drawable defaultCircle = mContext.getDrawable(R.drawable.shape_circle);
+        Drawable selectedIndicator = mContext.getDrawable(R.drawable.ic_baseline_check_24);
+        Drawable selectedCircle = mContext.getDrawable(R.drawable.shape_circle);
+        defaultCircle.mutate().setColorFilter(defaultIconColor, PorterDuff.Mode.SRC_IN);
+        selectedCircle.mutate().setColorFilter(selectedIconColor, PorterDuff.Mode.SRC_IN);
+        selectedCircle.setBounds(0, 0, 24, 24);
+
+        //Set standard content
+        itemViewHolder.senderIcon.setText(inbox.getFrom().substring(0,1));
+        itemViewHolder.senderName.setText(inbox.getFrom());
+        itemViewHolder.senderEmail.setText(inbox.getEmail());
+        itemViewHolder.emailPreview.setText(R.string.lorem_ipsum);
+        itemViewHolder.emailTimestamp.setText(inbox.getDate());
+
+        if (inbox.isSelected()) {
+            itemViewHolder.lyt_parent.setBackgroundColor(selectedColor);
+            itemViewHolder.senderIcon.setBackground(selectedCircle);
+            itemViewHolder.senderIcon.setText("");
+            itemViewHolder.senderIcon.setCompoundDrawablesRelativeWithIntrinsicBounds(selectedIndicator, null,null,null);
+        }
+        else {
+            itemViewHolder.lyt_parent.setBackgroundColor(Color.TRANSPARENT);
+            itemViewHolder.senderIcon.setBackground(defaultCircle);
+            itemViewHolder.senderIcon.setCompoundDrawablesWithIntrinsicBounds(null,null,null,null);
+
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.mItems.size();
+    }
+
+    public void addItem(int position, Inbox item){
+
+        mItems.add(position, item);
+        notifyItemInserted(position);
+    }
+
+    public void removeItem(int position){
+        if (position >= mItems.size()){
+            return;
+        }
+        mItems.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, getItemCount());
+    }
+
+    public void clearAllSelections() {
+        for (Inbox mItem : mItems) {
+            mItem.setSelected(false);
+        }
+        notifyDataSetChanged();
+    }
+
+    public void updateItems(List<Inbox> inboxList) {
+        this.mItems = inboxList;
+        notifyDataSetChanged();
+    }
+
+
+    private class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView senderIcon;
+        public TextView senderName;
+        public TextView senderEmail;
+        public TextView emailPreview;
+        public TextView emailTimestamp;
+        public View lyt_parent;
+
+
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            senderIcon = itemView.findViewById(R.id.sender_icon);
+            senderName = itemView.findViewById(R.id.name);
+            senderEmail = itemView.findViewById(R.id.email);
+            emailPreview = itemView.findViewById(R.id.info);
+            emailTimestamp = itemView.findViewById(R.id.date);
+            lyt_parent = itemView.findViewById(R.id.lyt_parent);
+
+            lyt_parent.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mListener.onItemClick(view, mItems.get(getLayoutPosition()), getLayoutPosition());
+                }
+            });
+//
+        }
+    }
+
+}
